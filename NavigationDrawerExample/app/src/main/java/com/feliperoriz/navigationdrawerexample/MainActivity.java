74f7603f73e07a3e20f272cf978940a086a71070
Felipe Roriz <froriz50@gmail.com>
2016-10-08 14:46:37 -0500

    initial commit

--- NavigationDrawerExample/app/src/main/java/com/feliperoriz/navigationdrawerexample/MainActivity.java
+++ NavigationDrawerExample/app/src/main/java/com/feliperoriz/navigationdrawerexample/MainActivity.java
@@ -1,13 +1,122 @@
 package com.feliperoriz.navigationdrawerexample;
 
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mNavigationDrawer;
+    private NavigationView mNavigationView;
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mNavigationDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigationView);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_container, new NavigationItem1Fragment())
+                .commit();
+
+        setupDrawerContent(mNavigationView);
+
+        mDrawerToggle = setupDrawerToggle();
+
+        mNavigationView.setCheckedItem(R.id.navigation_item_1);
+
+    }
+
+    // `onPostCreate` called when activity start-up is complete after `onStart()`
+    // NOTE! Make sure to override the method with only a single `Bundle` argument
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mNavigationDrawer, mToolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.navigation_item_1:
+                fragmentClass = NavigationItem1Fragment.class;
+                break;
+            case R.id.navigation_item_2:
+                fragmentClass = NavigationItem2Fragment.class;
+                break;
+            case R.id.navigation_item_3:
+                fragmentClass = NavigationItem3Fragment.class;
+                break;
+            case R.id.navigation_settings:
+                fragmentClass = NavigationSettingsFragment.class;
+                break;
+            default:
+                fragmentClass = NavigationItem1Fragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_container, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mNavigationDrawer.closeDrawers();
     }
 }
---
+++ NavigationDrawerExample/app/src/main/java/com/feliperoriz/navigationdrawerexample/NavigationItem2Fragment.java
@@ -0,0 +1,21 @@
+package com.feliperoriz.navigationdrawerexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by feliperoriz on 10/8/16.
+ */
+
+public class NavigationItem2Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_navigation_item2, container, false);
+    }
+}
